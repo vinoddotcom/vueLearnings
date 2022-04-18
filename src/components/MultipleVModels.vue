6411d6ea44a70256a87665fd1a7eb5e0263b8c9e
Vinod <vinod.yadav@vidyamantra.com>
2022-04-18 03:44:11 -0400

    first commit

---
+++ src/components/MultipleVModels.vue
@@ -0,0 +1,31 @@
+<template>
+password
+ <input type="password" value="password"  @input="updatePassword">
+ answer
+ <input type="text" value="adress" @input="updateAdress" >
+
+</template>
+
+<script>
+export default {
+props:{
+    password: String,
+    adress: String,
+},
+setup(props, context){
+
+    function updatePassword (){
+        context.emit("update:password", event.target.value)
+    }
+    function updateAdress(){
+          context.emit("update:adress", event.target.value)
+    }
+
+    return {updatePassword, updateAdress }
+}
+}
+</script>
+
+<style>
+
+</style>
